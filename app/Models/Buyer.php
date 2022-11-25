a5607de7efb6a43e3e3d2f43f50bc5a46050d263
roman <romannesterenko87@gmail.com>
2022-11-25 08:32:27 +0100

    add Buyer mvc

--- app/Models/Buyer.php
+++ app/Models/Buyer.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Buyer extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'name',
+        'last_name',
+        'phone',
+    ];
 }
--- database/migrations/2022_11_25_072109_create_buyers_table.php
+++ database/migrations/2022_11_25_072109_create_buyers_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('buyers', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('last_name');
+            $table->string('phone');
             $table->timestamps();
         });
     }
